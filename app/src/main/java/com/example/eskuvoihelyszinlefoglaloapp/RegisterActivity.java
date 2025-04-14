74f9b52ed3b42f99bc6a8ce89fc9e88af08ffc4a
Nagy Kristof <h267949@stud.u-szeged.hu>
2025-04-14 17:36:29 +0200

    firebase authentication

--- app/src/main/java/com/example/eskuvoihelyszinlefoglaloapp/RegisterActivity.java
+++ app/src/main/java/com/example/eskuvoihelyszinlefoglaloapp/RegisterActivity.java
@@ -7,16 +7,24 @@ import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
     private static final int SECRET_KEY = 99;
-
+    private static final String LOG_TAG = RegisterActivity.class.getName();
     private static final String PREF_KEY = MainActivity.class.getPackage().toString();
     private SharedPreferences preferences;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,12 @@ public class RegisterActivity extends AppCompatActivity {
             finish();
         }
         preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+    public void navigate_to_venues(View view) {
+        Intent intent = new Intent(this, VenueActivity.class);
+        startActivity(intent);
     }
     public void register(View view) {
         EditText usernameEditText = findViewById(R.id.edtusername);
@@ -40,7 +54,18 @@ public class RegisterActivity extends AppCompatActivity {
         String passwordStr = passwordEditText.getText().toString();
 
         Log.d(MainActivity.class.getName(), "Username: " + usernameStr +", Email: " + emailStr + ", Password: " + passwordStr);
-
+        mAuth.createUserWithEmailAndPassword(emailStr,passwordStr).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d(LOG_TAG,"User created successfully");
+                    navigate_to_login(view);
+                }else{
+                Log.d(LOG_TAG,"User creation failed");
+                Toast.makeText(RegisterActivity.this,"User creation failed"+ task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
     public void navigate_to_login(View view) {
         Intent intent = new Intent(this, loginActivity.class);
--- app/src/main/java/com/example/eskuvoihelyszinlefoglaloapp/VenueActivity.java
+++ app/src/main/java/com/example/eskuvoihelyszinlefoglaloapp/VenueActivity.java
@@ -1,15 +1,39 @@
 package com.example.eskuvoihelyszinlefoglaloapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseAuth;
 import java.util.ArrayList;
 import java.util.List;
 
 public class VenueActivity extends AppCompatActivity {
+    private static final String LOG_TAG = VenueActivity.class.getName();
+    private FirebaseUser user;
+    public void navigate_to_login(View view) {
+        Intent intent = new Intent(this, loginActivity.class);
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        View view = new View(this);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            Log.d(LOG_TAG,"Authenticated user");
+        }else{Log.d(LOG_TAG,"Authenticating user failed!"); navigate_to_login(view);}
+        // nem tudom jo e mert megorzi adatot
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venue);
 
